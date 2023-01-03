8f04438c672718457709caadca7cd02cf74eb751
Ross Beyer <rbeyer@rossbeyer.net>
2023-01-02 16:20:57 -0800

    feat(crater/cli_convert): Added a new --estimate_ages parameter which will cause craterconvert to do just that if ages are missing or zero in the input file.

--- src/python/synthterrain/crater/cli_convert.py
+++ src/python/synthterrain/crater/cli_convert.py
@@ -17,6 +17,8 @@ import sys
 import pandas as pd
 
 import synthterrain.crater as crater
+from synthterrain.crater.age import estimate_age_by_bin
+import synthterrain.crater.functions as crater_func
 import synthterrain.util as util
 
 
@@ -27,6 +29,16 @@ def arg_parser():
     parser = argparse.ArgumentParser(
         description=__doc__,
         parents=[util.parent_parser()],
+    )
+    parser.add_argument(
+        "--estimate_ages",
+        action="store_true",
+        help="When given, craters in the input file with no age specified, or an age "
+             "of zero, will have an age estimated based on their diameter and d/D "
+             "ratio using the Grun/Neukum production function and the VIPER "
+             "Environmental Specification equilibrium crater function.  Some resulting "
+             "craters may still yield a zero age if the d/D ratio was large relative "
+             "to the diameter, indicating a very fresh crater."
     )
     parser.add_argument(
         "infile",
@@ -50,7 +62,6 @@ def main():
 
     if args.infile.suffix.casefold() == ".csv":
         df = pd.read_csv(args.infile)
-        crater.to_file(df, args.outfile, xml=True)
 
     elif args.infile.suffix.casefold() == ".xml":
         df = pd.read_xml(args.infile, parser="etree", xpath=".//CraterData")
@@ -60,11 +71,24 @@ def main():
         df["d/D"] = df["freshness"] * 0.2731
         df["age"] = 0  # No age information from XML file format.
 
-        crater.to_file(df, args.outfile, xml=False)
     else:
         parser.error(
             f"The input file {args.infile} did not end in .csv or .xml."
         )
+
+    if args.estimate_ages:
+        a = df["diameter"].min()
+        b = df["diameter"].max()
+        pd_func = crater_func.GNPF(a=a, b=b)
+        eq_func = crater_func.VIPER_Env_Spec(a=a, b=b)
+        if "age" in df.columns:
+            df[df["age"] == 0] = estimate_age_by_bin(
+                df[df["age"] == 0], pd_func.csfd, eq_func.csfd, num=50
+            )
+        else:
+            df = estimate_age_by_bin(df, pd_func.csfd, eq_func.csfd, num=50)
+
+    crater.to_file(df, args.outfile, xml=(args.outfile.suffix.casefold() == ".xml"))
 
     return
 
